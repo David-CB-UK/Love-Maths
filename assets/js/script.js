baff8100d9801c6c66e1ca06447e1de603f1b622
DavidCB <david.corry_bass@yahoo.co.uk>
2026-06-25 15:55:29 +0100

    add favicon and tidy up code - added enter button functionality  (event function)

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    
     runGame("addition");
 
 });
@@ -24,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";   // Clears the answer box each time a new question is asked
+    document.getElementById("answer-box").focus();      // Puts the cursor in the answer box each time a new question is asked
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
