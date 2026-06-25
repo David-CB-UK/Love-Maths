ae3569e5f44497338588657cdf2fe671f9c64c72
DavidCB <david.corry_bass@yahoo.co.uk>
2026-06-25 15:03:34 +0100

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,12 +110,14 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";        // X better than '*' as most know x is times not an asterisk
 }
 
 function displayDivisionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "x";        // X better than '*' as most know x is times not an asterisk
+    document.getElementById('operator').textContent = "";       
 }
